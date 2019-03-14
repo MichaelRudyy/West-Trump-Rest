3e676a3948810118637d1e4a9fc85cf5f0a5e2c0
Michael Rudyy <mcrudyy@gmail.com>
2019-03-14 18:25:04 +0200

    Refactore

--- src/main/java/com/mcrudyy/WestTrumpDialog/model/west/WestQuote.java
+++ src/main/java/com/mcrudyy/WestTrumpDialog/model/west/WestQuote.java
@@ -4,7 +4,6 @@ package com.mcrudyy.WestTrumpDialog.model.west;
 import com.mcrudyy.WestTrumpDialog.model.Quote;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 import java.io.Serializable;
 
@@ -13,23 +12,4 @@ import java.io.Serializable;
 @EqualsAndHashCode(callSuper = true)
 public class WestQuote extends Quote implements Serializable {
     private String id;
-
-/*    public WestQuote() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "WestQuote{" +
-                "id='" + id + '\'' +
-                ", message='" + message + '\'' +
-                '}';
-    }*/
 }
